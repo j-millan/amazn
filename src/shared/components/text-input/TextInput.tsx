c77c317e31449de6c5be1e844df8eef80e190b33
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-11-18 21:20:40 -0400

    Add errors to text input component

--- src/shared/components/text-input/TextInput.tsx
+++ src/shared/components/text-input/TextInput.tsx
@@ -1,34 +1,46 @@
+import { FieldError } from "react-hook-form";
 import styles from "./TextInput.module.css";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
 
 export interface TextInputProps {
+  name: string;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  register: Function;
   label: string;
   placeholder?: string;
-  minLength?: number;
-  maxLength?: number;
+  error?: FieldError;
 }
 
-export const TextInput = ({ label, placeholder, minLength, maxLength }: TextInputProps) => {
-  const NAME = label.toLowerCase();
-
+export const TextInput = ({
+  name,
+  register,
+  label,
+  placeholder,
+  error,
+}: TextInputProps) => {
   return (
     <div className={styles.textInput}>
       {/* Label */}
-      <label className={styles.label} htmlFor={NAME}>
+      <label className={styles.label} htmlFor={name}>
         {label}
       </label>
       {/* Label */}
 
       {/* Input */}
       <input
-        className={styles.input}
+        className={`${styles.input} ${error && styles.hasError}`}
         type="text"
-        name={NAME}
-        id={NAME}
-        minLength={minLength}
-        maxLength={maxLength}
         placeholder={placeholder}
+        {...register(name)}
       />
       {/* Input */}
+
+      {error && (
+        <span className={styles.error}>
+          <AiOutlineExclamationCircle size={14} />
+          <span className={styles.message}>{error?.message}</span>
+        </span>
+      )}
     </div>
   );
 };
