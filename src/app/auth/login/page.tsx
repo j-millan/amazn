a9a2eda25a5f8f24c7c4d58863e88d1e553727bf
Jhonattan Millán <jhonattan.millan504@gmail.com>
2024-11-18 21:21:42 -0400

    Add schema based validation to login form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,15 +1,53 @@
+"use client";
+
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+
 import { Button, ButtonColorEnum, ButtonSizeEnum, TextInput } from "@/shared";
 import styles from "./page.module.css";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface LoginFormInterface {
+  email: string;
+  password: string;
+}
+
+const LOGIN_SCHEMA = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  })
+  .required();
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInterface>({ resolver: yupResolver(LOGIN_SCHEMA) });
+  const onSubmit: SubmitHandler<LoginFormInterface> = (data) => {
+    console.debug(data);
+    console.debug(errors.email);
+  };
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Login</h2>
 
       {/* Form */}
-      <form className={styles.form}>
-        <TextInput label="Email or mobile phone number" />
-        <TextInput label="Password" />
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <TextInput
+          label="Email or mobile phone number"
+          name="email"
+          register={register}
+          error={errors.email}
+        />
+        <TextInput
+          label="Password"
+          name="password"
+          register={register}
+          error={errors.password}
+        />
         <Button block={true} size={ButtonSizeEnum.SM}>
           Log in
         </Button>
