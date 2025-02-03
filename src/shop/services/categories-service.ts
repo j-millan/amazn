9a454fa37ddcaf216d1a4929172459421ddec6d5
Jhonattan Millán <jmillan@w3itsolutions.net>
2025-02-03 11:10:35 -0400

    Remove console log

--- src/shop/services/categories-service.ts
+++ src/shop/services/categories-service.ts
@@ -4,7 +4,6 @@ import { CategoryResponseDto } from "../dto/category.dto";
 class CategoriesService {
   async getCategories(): Promise<CategoryResponseDto[]> {
     const response = await Axios.get<CategoryResponseDto[]>("/categories");
-    console.debug(response);
     return response.data;
   }
 }
