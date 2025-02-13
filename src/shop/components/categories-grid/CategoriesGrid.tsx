ad007e8d7e62b3811c2c8416df3294f7f0d971ed
Jhonattan Millán <jmillan@w3itsolutions.net>
2025-02-13 02:10:56 -0400

    Add loading to categories grid component

--- src/shop/components/categories-grid/CategoriesGrid.tsx
+++ src/shop/components/categories-grid/CategoriesGrid.tsx
@@ -1,16 +1,23 @@
-'use client';
+"use client";
 
 import { CategoryCard, useCategories } from "@/shop";
+import { Spinner, SpinnerSizeEnum } from "@/shared";
 import styles from "./CategoriesGrid.module.css";
 
 export const CategoriesGrid = () => {
-  const categories = useCategories();
+  const { categories, loading } = useCategories();
 
   return (
     <div className={styles.categoriesGrid}>
-      {categories.map((category) => (
-        <CategoryCard category={category} key={category.id} />
-      ))}
+      {loading ? (
+        <Spinner size={SpinnerSizeEnum.LG} />
+      ) : (
+        <div className={styles.grid}>
+          {categories.map((category) => (
+            <CategoryCard category={category} key={category.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/shop/hooks/use-categories.tsx
+++ src/shop/hooks/use-categories.tsx
@@ -5,16 +5,23 @@ import { useEffect, useState } from "react";
 import { categoriesService } from "../services/categories-service";
 import { CategoryResponseDto } from "../dto/category.dto";
 
-export const useCategories = () => {
+interface UseCategoriesReturn {
+  categories: CategoryResponseDto[];
+  loading: boolean;
+}
+
+export const useCategories = (): UseCategoriesReturn => {
   const [categories, setCategories] = useState(
     new Array<CategoryResponseDto>()
   );
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     categoriesService.getCategories().then((response) => {
       setCategories(response);
+      setLoading(false);
     });
   }, []);
 
-  return categories;
+  return { categories, loading };
 };
