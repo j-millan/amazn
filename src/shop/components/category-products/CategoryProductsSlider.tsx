e2cf72210f6370365ee9f9407eb028eb83846dd2
Jhonattan Millán <jmillan@w3itsolutions.net>
2025-02-18 01:15:43 -0400

    Add link to category in category products slider component

--- src/shop/components/category-products/CategoryProductsSlider.tsx
+++ src/shop/components/category-products/CategoryProductsSlider.tsx
@@ -1,6 +1,6 @@
 import { CategoryInterface } from "@/shop/interfaces/category.interface";
 import { ProductInterface } from "@/shop/interfaces/product.interface";
-import { ProductCard } from "../product-card/ProductCard";      
+import { ProductCard } from "../product-card/ProductCard";
 import styles from "./CategoryProductsSlider.module.css";
 
 interface CategoryProductsSliderProps {
@@ -12,17 +12,31 @@ export const CategoryProductsSlider = ({
   category,
   products,
 }: CategoryProductsSliderProps) => {
+  const { id, description, slug } = category;
+  const categoryUrl = `/categories/${id}/${slug}`;
+
   return (
     <div className={styles.categoryProducts}>
-      <h3 className={styles.description}>{category.description}</h3>
+      <h3>{description}</h3>
+
+      {/* Help text */}
       <span className={styles.helpText}>
-        Check each product page for more buying options.
+        <span className={styles.message}>
+          Check each product page for more buying options.
+        </span>
+        <a href={categoryUrl} className={`${styles.seeMore} link`}>
+          See more products
+        </a>
       </span>
+      {/* Help text */}
+
+      {/* Slider */}
       <div className={styles.slider}>
         {products.map((product) => (
           <ProductCard product={product} key={product.id} />
         ))}
       </div>
+      {/* Slider */}
     </div>
   );
 };
